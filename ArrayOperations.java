c88f8ded078ee84540b33c2b7f0bbb9c1f952b16
HARDIK GUPTA <parnami.hardik50@gmail.com>
2020-08-07 20:10:17 +0530

    Project is ready to release with first operation : Array Traversal

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -1,19 +1,21 @@
 public class ArrayOperations
 {
-	public static void main(String args...)
+	public static void main(String args[])
 	{
 		int[] array={1,3,5,7,9,2,4,6,8};
+		Operations operations=new Operations();
+		operations.traverseArray(array);
 	}
 	
 	
 }
-public class Operations
+class Operations
 {
-	void TraverseArray(int[] array)
+	void traverseArray(int[] array)
 	{
 		for(int i=0;i<array.length;i++)
 		{
-			System.out.println("Elements at Position "+i+1+" is "+array[i]);
+			System.out.println("Elements at Position "+i+" is "+array[i]);
 		}
 	}
 }
